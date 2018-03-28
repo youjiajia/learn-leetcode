bd3aa84529eacd9f558970d7b6a55031dd0683cd
JasonYou <jasonyou.info@gmail.com>
2018-03-29 00:11:43 +0800

    sort

--- basis/radix.py
+++ basis/radix.py
@@ -12,5 +12,18 @@ def LSDSort(array, d):
         i += 1
     return array
 
+def generate(array, i, d):
+    if i < 0:
+        return array
+    s = [[] for _ in xrange(10)]
+    for x in array:
+        s[x/(10**i)%10].append(x)
+    s = [generate(x, i-1, d) for x in s]
+    array = [y for x in s for y in x]
+    return array
 
-print LSDSort([1,3,5,9,4,2,7,3,1], 1)
+def MSDSort(array, d):
+    return generate(array, d, d)
+
+print LSDSort([1,34,5,92,4,2,7,3,133], 3)
+print MSDSort([1,34,5,92,4,2,7,3,133], 3)
